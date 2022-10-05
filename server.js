6f261add60ce601e49e4a194079d6ce1bcd1a124
Glitch (arras-template) <none>
2022-10-05 10:11:04 +0000

    🌱🎙 Checkpoint ./lib/definitions.js:48077357/46 ./server.js:48077357/2

--- server.js
+++ server.js
@@ -6845,7 +6845,7 @@ var maintainloop = (() => {
     return () => {
         // Do stuff
         makenpcs();      
-        // makefood(); 
+         makefood(); 
         // Regen health and update the grid
         entities.forEach(instance => {
             if (instance.shield.max) {
