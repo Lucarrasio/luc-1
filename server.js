f30574238dc1bda6f6a3762ec9f08b30a967305d
Glitch (arras-template) <none>
2022-10-21 18:26:41 +0000

    🎳🌉 Checkpoint ./server.js:48077357/11 ./lib/definitions.js:48077357/86

--- server.js
+++ server.js
@@ -6817,7 +6817,7 @@ var maintainloop = (() => {
         if (ran.chance(3 -  0.5 * census.crasher / room.maxFood / room.nestFoodAmount)) {
             let spot, i = 30;
             do { spot = room.randomType('nest'); i--; if (!i) return 0; } while (dirtyCheck(spot, 100));
-              let type = (ran.dice(2)) ? ran.choose([Class.square]) : Class.crasher;
+              let type = (ran.dice(2)) ? ran.choose([Class.egg23]) : Class.crasher;
           //let type = (ran.dice(10000)) ? ran.choose([Class.sentryGun, Class.sentrySwarm, Class.sentryTrap, Class.semicrusher, Class.arrowCrasher, Class.crusher, Class.visDestructia, Class.destroyerCrasher, Class.triblade, Class.flashCrasher, Class.grouper, Class.megaCrasher, Class.swimmer, Class.visLITE,  Class.dreadnoughtLITE, Class.colliderLITE, Class.gunshipLITE, Class.detraLITE]) : Class.crasher;
             let o = new Entity(spot);
                 o.define(type);
